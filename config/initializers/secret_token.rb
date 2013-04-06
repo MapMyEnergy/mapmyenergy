d07e3cbfddc6c6ed7e08110789dac2c4b6748ebf
Hao Chen <detectx@gmail.com>
2013-04-06 12:21:52 -0400

    We're going with rails people

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapmyenergy::Application.config.secret_token = '22b5e0c00898cf2dc29539719e71560e56e9aa059f40aa881d2669abdfba21e63cf23a15de0c32434a815671833cdacc9cf7211ef7771abbf918886aea8ea106'
